fd1be8a0974330ac0e7a1daf57b011f5fdff831f
V1CT0R-06 <moraisvictor201106@gmail.com>
2025-10-24 12:52:45 +0100

    fp1.5

--- FP/aulas/aula05/minmax.py
+++ FP/aulas/aula05/minmax.py
@@ -14,27 +14,25 @@ def inputFloatList():   #defenir função para escrever a lista
         else:           # Converte o input e armazena na lista
             float_list += [float(user_input)]  
             
-        
     return float_list
 
+
 def minmax(lst):
-    if not lst:         # Verifica se a lista está vazia
-        return None, None  # Retorna None se a lista estiver vazia
+    if not lst:  # lista vazia     
+        return None, None  
 
-    # Inicializa o mínimo e máximo com o primeiro elemento da lista
-    minimum = maximum = lst[0]
+    minimum = lst[0]
+    maximum = lst[0]
 
-    # Percorre a lista a partir do segundo elemento
     for num in lst[1:]:
         if num < minimum:
-            minimum = num   # se o valor for menor que o primeiro elemento, o min muda para esse valor
+            minimum = num   
         if num > maximum:
-            maximum = num   # se o valor for maior que o primeiro elemento, o max muda para esse valor 
+            maximum = num   
             
     return minimum, maximum # min e max após a função
 
-lista = inputFloatList()        # lista é o input da lista da primeira função
-minimo, maximo = minmax(lista)  # função para o mínimo e o máximo
+minimo, maximo = minmax(inputFloatList())  
 
 print(f"Mínimo: {minimo}, Máximo: {maximo}")
 
