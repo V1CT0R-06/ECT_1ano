b9c6e7169238fbbb72af190829c7f4bec70a55d5
V1CT0R-06 <moraisvictor201106@gmail.com>
2025-10-24 12:54:46 +0100

    fp1.5

--- FP/aulas/aula04/fibonacci.py
+++ FP/aulas/aula04/fibonacci.py
@@ -5,24 +5,3 @@ def fibonacci(n):
     return fib
 
 print(fibonacci(10))
-
-def Fibonacci(n):
-    # Casos base:
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-
-    # valores iniciais
-    a = 0  # F0
-    b = 1  # F1
-
-    # vamos calcular até chegar a Fn
-    for i in range(2, n + 1):
-        proximo = a + b  # soma dos dois anteriores
-        a = b            # anda um passo
-        b = proximo      # anda um passo
-
-    return b
-
-print(Fibonacci(10))
